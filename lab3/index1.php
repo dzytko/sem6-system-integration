536c0322a1e4ecf4944aa624e00293ccda7ccdfa
dzytko <dawid.zytko@gmail.com>
2022-03-19 20:35:22 +0100

    lab3 task 1-4

---
+++ lab3/index1.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "127.0.0.1";
+$username = "sakila1";
+$password = "pass";
+$database = "sakila";
+
+$conn = new mysqli($servername, $username, $password, $database);
+if ($conn->connect_error)
+    die("Database connection failed: " . $conn->connect_error);
+
+echo "Databse connected successfully, username " . $username . "<br><br>";
+
+$sql = "SELECT actor_id, first_name, last_name FROM actor ORDER BY first_name";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "id: " . $row["actor_id"] . " - Name: " . $row["first_name"] . " " . $row["last_name"] . "<br>";
+    }
+}
+else {
+    echo "0 results";
+}
+$conn->close();
+
+
---
+++ lab3/update-adam.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "127.0.0.1";
+$username = "sakila2";
+$password = "pass";
+$database = "sakila";
+
+$conn = new mysqli($servername, $username, $password, $database);
+if ($conn->connect_error)
+    die("Database connection failed: " . $conn->connect_error);
+
+echo "Databse connected successfully, username " . $username . "<br><br>";
+$sql = "UPDATE actor SET first_name = 'CHRIS' WHERE first_name = 'ADAM'";
+$conn->query($sql);
+echo "Table actor updated";
+
+$conn->close();
