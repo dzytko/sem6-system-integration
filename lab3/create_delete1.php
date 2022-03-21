f402ec751e7a93122b2614d748f845bd8830e93d
dzytko <dawid.zytko@gmail.com>
2022-03-21 16:42:57 +0100

    lab 3 final

--- lab3/create_delete1.php
+++ lab3/create_delete1.php
@@ -9,8 +9,12 @@ if ($conn->connect_error)
     die("Database connection failed: " . $conn->connect_error);
 
 echo "Databse connected successfully, username " . $username . "<br><br>";
-$sql = "INSERT INTO ";
+$sql = "INSERT INTO actors (first_name, last_name) VALUES ('name', 'lastname')";
 $conn->query($sql);
-echo "Table actor updated";
+echo "Table actor added";
+
+$sql = "DELETE FROM actors WHERE name='name'";
+$conn->query($sql);
+echo "Table actor added";
 
 $conn->close();
