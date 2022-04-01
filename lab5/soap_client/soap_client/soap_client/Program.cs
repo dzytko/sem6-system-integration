cf56b98864af419ec358ac766efa7c4b4342c7c5
dzytko <dawid.zytko@gmail.com>
2022-04-01 17:53:42 +0200

    lab 5 task 3

---
+++ lab5/soap_client/soap_client/soap_client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Connections;
+using MyFirstSOAPInterface;
+
+namespace soap_client
+{
+    public static class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Console.WriteLine("My First SOAP Client!");
+            var client = new MyFirstSOAPInterfaceClient();
+            
+            var text = await client.getHelloWorldAsStringAsync("Karol");
+            var daysBetweenDates = await client.getDaysBetweenDatesAsync(
+                new DateTime(2019, 1, 1).ToString("dd MM yyyy"),
+                new DateTime(2019, 5, 20).ToString("dd MM yyyy")
+            );
+            
+            Console.WriteLine(text);
+            Console.WriteLine($"Days between dates: {daysBetweenDates}");
+        }
+    }
+}
--- lab5/soap_server/src/com/soapsoapsoap/MyFirstWS.java
+++ lab5/soap_server/src/com/soapsoapsoap/MyFirstWS.java
@@ -1,21 +1,23 @@
 package com.soapsoapsoap;
+
 import javax.jws.WebService;
 import java.time.Duration;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @WebService(endpointInterface = "com.soapsoapsoap.MyFirstSOAPInterface")
-public class MyFirstWS implements MyFirstSOAPInterface{
+public class MyFirstWS implements MyFirstSOAPInterface {
     public String getHelloWorldAsString(String nazwa) {
-        return "Witaj "+nazwa+"!";
+        return "Witaj " + nazwa + "!";
     }
+
     public long getDaysBetweenDates(String date1, String date2) {
-        long numberOfDays=0;
+        long numberOfDays = 0;
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd MM yyyy");
         try {
-            LocalDateTime tempDate1 = LocalDateTime.parse(date1, dtf);
-            LocalDateTime tempDate2 = LocalDateTime.parse(date2, dtf);
-            numberOfDays = Duration.between(tempDate1, tempDate2).toDays();
+            LocalDate tempDate1 = LocalDate.parse(date1, dtf);
+            LocalDate tempDate2 = LocalDate.parse(date2, dtf);
+            numberOfDays = Duration.between(tempDate1.atStartOfDay(), tempDate2.atStartOfDay()).toDays();
         } catch (Exception e) {
             e.printStackTrace();
         }
